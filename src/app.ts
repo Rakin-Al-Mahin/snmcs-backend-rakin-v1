a29c2ee3c45116f8f3d190bff8d157a41757ecd2
Mahin <rakin99raj@gmail.com>
2024-10-26 01:47:32 +0600

    test

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 //app.ts
+import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
 const app: Application = express();
-import cors from 'cors';
 
 app.use(cors());
 
@@ -12,5 +12,5 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
-
+//mahin
 export default app;
